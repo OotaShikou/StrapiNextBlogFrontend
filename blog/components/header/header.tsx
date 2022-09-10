18da52618d420a33a3fcb207ae95d2ed7b91d29c
shikou_ota <s.oota.grune@gmail.com>
2022-09-10 19:33:18 +0900

    init

---
+++ blog/components/header/header.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+    return (
+      <div >
+        ss
+      </div>
+    )
+  }
+  
+  export default Home
--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Stack, Button } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      ...
+      <h1 className="text-blue-600 md:text-green-600">
+        Hello world!
+      </h1>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
